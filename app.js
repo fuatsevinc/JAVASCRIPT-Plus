0f64338e09928780a81ae0cf491caf489868712c
fuatsevinc <fuatsevinc@icloud.com>
2022-08-22 20:02:26 +0200

    update

--- app.js
+++ app.js
@@ -106,3 +106,59 @@ if (data.borders){
   
   viewCountry('Turkey');
   // console.log(globalData);
+
+// ? ===============================================
+// ?               FONKSİYONLAR
+// ? ===============================================
+
+// !------------------------------------------------
+// ! 3.YONTEM  : ARROW FUNCTION
+// !------------------------------------------------
+
+console.log("**** ARROW ******");
+
+//* ORNEK: Silindir Hacmi
+//************************************************/
+
+const r = Number(prompt("yaricap giriniz:"));
+const h = Number(prompt("yukseklik giriniz:"));
+
+const hacimHesapla = (r, h) => Math.PI * r * r * h;
+
+// console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toExponential()}`);
+console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toFixed(2)}`);
+
+//* ORNEK: Yas hesapla
+//************************************************/
+
+const tarih = Number(prompt("Dogum Tarihini giriniz:"));
+
+//! Arrow Func yontemi ile tanimlama
+const yasHesapla = (tarih) => {
+  const yas = new Date().getFullYear() - tarih;
+  return yas;
+};
+console.log("YASINIZ:" + yasHesapla(tarih));
+
+//?-----------------------------------------
+
+// console.log("YASINIZ:" + yasHesapla2(tarih));
+//!Funct expression ve arrow func yontemlerinde
+//! Once fonks tanimlanmalidir sonra cagrilmalidir.
+//! Aksi takdirde hata alrsiniz.
+
+//! Func Expression yontemi ile tanimlama
+const yasHesapla2 = function (tarih) {
+  const yas = new Date().getFullYear() - tarih;
+  return yas;
+};
+//?-----------------------------------------
+
+console.log("YASINIZ:" + yasHesapla3(tarih));
+
+//! Func Declaration yontemi ile tanimlama
+function yasHesapla3(tarih) {
+  const yas = new Date().getFullYear() - tarih;
+  return yas;
+}
+//?-----------------------------------------
